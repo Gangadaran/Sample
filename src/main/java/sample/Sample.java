739527971957b08b28c2e5134031f5d7b78e6961
Gangadaran <sgangadaran1999@gmail.com>
2023-10-28 14:37:49 +0530

    changed

--- src/main/java/sample/Sample.java
+++ src/main/java/sample/Sample.java
@@ -1,12 +1,10 @@
 package sample;
 
 public class Sample {
-	private String name = "Gangadaran";
+	private String name = "Ganga";
 	private int a = 1000;
 	
 	private int sample() {
 	   return a;
 
 	}
-
-}
